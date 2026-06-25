0d8fbb19b722b32e8b03f1a2f5e33ddb65cc42b0
TihayaKousaka <2939723848@qq.com>
2026-06-25 14:10:49 +0800

    update server parameter

--- rlinf/models/embodiment/openpi/dataconfig/realworld_dataconfig.py
+++ rlinf/models/embodiment/openpi/dataconfig/realworld_dataconfig.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import dataclasses
+import pathlib
 
 import numpy as np
 import openpi.models.model as _model
